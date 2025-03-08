b6b1b3175fd4c09a1f586800f13da70a9cdad4b9
israe <israe@DESKTOP-SSJS3NH>
2025-03-08 13:04:05 -0300

    Agenda 03

--- MauiAppMinhaCompras1/App.xaml.cs
+++ MauiAppMinhaCompras1/App.xaml.cs
@@ -1,12 +1,28 @@
-﻿namespace MauiAppMinhaCompras1
+﻿using MauiAppMinhaCompras123.Helpers;
+using SQLite;
+
+namespace MauiAppMinhaCompras1
 {
     public partial class App : Application
     {
-        public App()
-        {
-            InitializeComponent();
+        static SQLiteDatabaseHelper _db;
 
-            MainPage = new AppShell();
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
         }
     }
 }
